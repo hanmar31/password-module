408cfcc4aa4f10e9d195af9db0c49896e7887790
Hanna Mårtensson <hm233dq@student.lnu.se>
2026-09-23 10:18:46 +0200

    Add methods for minLength and uppercase character requirement

---
+++ src/PasswordValidator.js
@@ -0,0 +1,37 @@
+/**
+ * @author Hanna Mårtensson
+ * @version 1.0.0
+ */
+
+/**
+ * Validates passwords based on various rules.
+ */
+class PasswordValidator {
+  /**
+   * Checks the password agains the minimum length required.
+   *
+   * @param {string} password - The password to check.
+   * @param {number} minLength - The minimum required password length.
+   * @returns {boolean} True if password meets required minimum length. otherwise false.
+   */
+  hasMinLength (password, minLength) {
+    return password.length >= minLength
+  }
+
+  /**
+   * Checks if the passsword contains a uppercase character.
+   *
+   * @param {string} password - The password to check.
+   * @returns {boolean} True if the password contains a uppercase character.
+   */
+  hasUpperCase (password) {
+    for (const letter of password) {
+      if (letter === letter.toUpperCase() && letter !== letter.toLowerCase()) {
+        return true
+      }
+    }
+    return false
+  }
+}
+
+export { PasswordValidator }
